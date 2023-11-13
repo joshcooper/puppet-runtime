9ffeb82dcc90ca4ac9ab2c791bdf0afccc319ba4
Josh Cooper <joshcooper@users.noreply.github.com>
2023-11-13 13:36:15 -0800

    Use pl-autotools on sles 11
    The vendor provided autoconf and automake:
    
        # autoconf --version | head -1
        autoconf (GNU Autoconf) 2.63
    
        # automake --version | head -1
        automake (GNU automake) 1.10.1
    
    are too old to build ruby 3.2, so install pl-autotools instead:
    
        # /opt/pl-build-tools/bin/autoconf --version | head -1
        autoconf (GNU Autoconf) 2.69
        # /opt/pl-build-tools/bin/automake --version | head -1
        automake (GNU automake) 1.15

--- configs/platforms/sles-11-x86_64.rb
+++ configs/platforms/sles-11-x86_64.rb
@@ -6,13 +6,12 @@ platform "sles-11-x86_64" do |plat|
   plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/sles/11/x86_64/pl-build-tools-sles-11-x86_64.repo"
   packages = [
     "aaa_base",
-    "autoconf",
-    "automake",
     "gcc",
     "java-1_7_1-ibm-devel",
     "libbz2-devel",
     "make",
     "pkgconfig",
+    "pl-autotools",
     "pl-cmake",
     "pl-gcc",
     "readline-devel",
