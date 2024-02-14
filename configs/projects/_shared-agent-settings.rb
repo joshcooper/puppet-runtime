fa3c173673adfe5eff3fa3549a2dddd501fbd475
Josh Cooper <joshcooper@users.noreply.github.com>
2024-02-14 07:27:12 -0800

    Harden binaries on EL9 and Debian 11 & 12

--- configs/projects/_shared-agent-settings.rb
+++ configs/projects/_shared-agent-settings.rb
@@ -152,7 +152,13 @@ proj.setting(:ldflags, "-L#{proj.libdir} -L/opt/pl-build-tools/lib -Wl,-rpath=#{
 # stack canary and full RELRO.
 # We only do this on platforms that use their default OS toolchain since pl-gcc versions
 # are too old to support these flags.
-if platform.name =~ /sles-15|el-8|debian-10/ || platform.is_fedora?
+# REMIND platform.is_amazon
+# REMIND: platform.is_osx?
+# REMIND: platform.is_ubuntu?
+if platform.is_debian? ||
+   platform.is_fedora? ||
+   (platform.is_el? && platform.os_version.to_i >= 8) ||
+   (platform.is_sles? && platform.os_version.to_i >= 15)
   proj.setting(:cppflags, "-I#{proj.includedir} -D_FORTIFY_SOURCE=2")
   proj.setting(:cflags, '-fstack-protector-strong -fno-plt -O2')
   proj.setting(:ldflags, "-L#{proj.libdir} -Wl,-rpath=#{proj.libdir},-z,relro,-z,now")
